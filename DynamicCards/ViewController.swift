1a5eb62bb63ff0d484cd5923b958f2e26400d561
salimbraksa <salim.braksa@gmail.com>
2015-12-02 20:45:36 +0000

    Works

--- DynamicCards/ViewController.swift
+++ DynamicCards/ViewController.swift
@@ -7,41 +7,50 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+   @IBOutlet weak var tableView: UITableView!
+   
+   var images = [
+      "http://media.engadget.com/img/product/4/37g/apple-macbook-pro-13-inch-unibody-4g6-800.jpg",
+      "http://www.ethicalconsumer.org/portals/0/images/reports/244634_large.jpg"
+   ]
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
 
-    @IBOutlet weak var tableView: UITableView!
-    
-    var images = [
-        "http://media.engadget.com/img/product/4/37g/apple-macbook-pro-13-inch-unibody-4g6-800.jpg",
-        "http://www.ethicalconsumer.org/portals/0/images/reports/244634_large.jpg"
-    ]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.tableView.registerNib(UINib(nibName: "CardTableViewCell", bundle: nil), forCellReuseIdentifier: "CardTableViewCell")
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1;
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier("CardTableViewCell", forIndexPath: indexPath) as! CardTableViewCell
-        return cell
-    }
-    
-    
-
+      
+      self.tableView.registerNib(UINib(nibName: "CardTableViewCell", bundle: nil), forCellReuseIdentifier: "CardTableViewCell")
+      
+      tableView.estimatedRowHeight = 430
+      tableView.rowHeight = UITableViewAutomaticDimension
+   }
+   
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+   
+   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return 3
+   }
+   
+   func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+      return 1;
+   }
+   
+   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+      
+      let cell = tableView.dequeueReusableCellWithIdentifier("CardTableViewCell", forIndexPath: indexPath) as! CardTableViewCell
+      
+      cell.imageViewHeightConstraint.constant = CGFloat( random() % 300 )
+      
+      return cell
+   }
+   
+   
+   
 }
 
